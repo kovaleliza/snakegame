1654ca93ae16d2cd65f8bf17acf44abb95404d9a
kovaleliza <kovaleliza11@gmail.com>
2026-03-17 16:19:31 +0100

    new

---
+++ gamepanel.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JPanel;
+
+public class gamepanel extends JPanel{
+
+    gamepanel(){
+
+    }
+
+    public void startgame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+
+
+
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        public void keyPressed(KeyEvent e){
+
+        }
+
+
+    }
+
+}
