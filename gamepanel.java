8cdcf57aff53102dd4ba9ce6ec0031a85cded09d
kovaleliza <kovaleliza11@gmail.com>
2026-03-17 18:07:50 +0100

    update

--- gamepanel.java
+++ gamepanel.java
@@ -4,7 +4,29 @@ import javax.swing.JPanel;
 
 public class gamepanel extends JPanel{
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyparts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+        Timer timer;
+        Random random;
+
+
+
+
     gamepanel(){
+    random = new Random();
+    this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+    this.setBackground(Color.black);
+    this.setFocusable(true);
 
     }
 
